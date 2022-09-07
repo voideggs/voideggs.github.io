45f8cb16a8dec2402e3fb1a0727605feb8934718
voideggs <110767645+voideggs@users.noreply.github.com>
2022-09-07 12:44:16 +1000

    Add files via upload

---
+++ assignment2/exercise2/exercise2/modeSwitch.js
@@ -0,0 +1,16 @@
+let isDarkMode = false;
+
+function switchModes(){
+    if(isDarkMode === false){
+        document.documentElement.style.setProperty("--col-01", "#050306");
+        document.documentElement.style.setProperty("--col-02", "#faf8f4");
+        document.getElementById("modeButton").innerHTML = "Light Mode";
+        isDarkMode = true;
+    } else {
+        document.documentElement.style.setProperty("--col-01", "#faf8f4");
+        document.documentElement.style.setProperty("--col-02", "#050306");
+        document.getElementById("modeButton").innerHTML = "Dark Mode";
+        isDarkMode = false;
+    }
+}
+
